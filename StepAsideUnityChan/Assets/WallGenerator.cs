b40fa38ba2d384f0e1a504be0f20f5bd4829bceb
knowledge0212 <knowledge0212@gmail.com>
2018-04-18 18:21:32 +0900

    Lesson6の発展課題を対応しました。最初に壁を一定数作成し、Unityちゃんが壁を通り過ぎたらItemを作成する処理にしました。

---
+++ StepAsideUnityChan/Assets/WallGenerator.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallGenerator : MonoBehaviour {
+
+	//wallPrefabを入れる
+	public GameObject wallPrefab;
+
+	// Use this for initialization
+	void Start () {
+		addWall ();
+	}
+
+	// Item作成を判定する壁を作成する
+	private void addWall(){
+		int wallStartPos = -240;
+		int wallEndPos = 80;
+		for(int i=wallStartPos; i < wallEndPos; i+=40){
+			GameObject wall = Instantiate (wallPrefab) as GameObject;
+			wallPrefab.transform.position = 
+				new Vector3 (wall.transform.position.x,wall.transform.position.y,i);
+		}
+	}
+}
